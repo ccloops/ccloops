b25834224ccda9c5a8a53907f3c8568d43dde491
Catherine Looper <loopercat@gmail.com>
2018-03-27 14:25:56 -0700

    adding retro font and blink toggle

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,12 +1,27 @@
 import './_landing.scss';
 import React from 'react';
-import Resume from '../../assets/resume.pdf';
+import Signature from '../../assets/signature.jpg';
 
 class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBlinking: true,
+    };
+
+    this.toggleBlink = this.toggleBlink.bind(this);
+  }
+
+  toggleBlink() {
+    this.setState({isBlinking: !this.state.isBlinking});  
+  }
+
   render() {
+    console.log(this.state);
     return (
-      <div className='resume'>
-        <iframe src={Resume} frameBorder="0"></iframe>
+      <div> 
+        <h1 className={this.state.isBlinking ? 'toggleBlink' : null} id="blink" onClick={this.toggleBlink}>Catherine Looper</h1>
+        <img src={Signature} alt="signature" />
       </div>
     );
   }
