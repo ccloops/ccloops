15b5f33369b50fd9195fc21a8fab3217042798af
Catherine Looper <loopercat@gmail.com>
2018-03-28 14:09:33 -0700

    adjusted image styling for projects

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -4,24 +4,18 @@ import React from 'react';
 import Clouds from '../../assets/cloud-level-overview.png';
 import Quiz from '../../assets/quiz-fellows.png';
 import OhRose from '../../assets/oh-rose.png';
+import Trendly from '../../assets/Trendly-flowchart.png';
+import Todo from '../../assets/todo-list-overview.png';
 
 export default class Projects extends React.Component {
   render() {
     return (
-      <div>
-        <ul className='projects'>
-          <a target='blank' href='https://geo-jumper-frontend.herokuapp.com/'><li>Geo-Jumper</li></a>
-          <a target='blank' href='https://github.com/Meetup-Trendly/trendly-server'><li>Trendly</li></a>
-          <a target='blank' href='https://ccloops.github.io/quiz-fellows/'><li>Quiz Fellows</li></a>
-          <a target='blank' href='https://github.com/ccloops/28-todo-app/tree/master/lab-catherine'><li>To Do List</li></a>
-          <a target='blank' href='https://ccloops.github.io/oh-rose/'><li>Oh, Rose</li></a>
-
-          <div className='circle-container'>
-            <a href='#' className='center'><img src={Clouds} alt="cloud-level" /></a>
-            <a href='#' className='deg0'><img src={Quiz} alt="quiz fellows" /></a>
-            <a href='#' className='deg45'><img src={OhRose} alt="Oh, Rose" /></a>
-          </div>
-        </ul>
+      <div className='projects'>
+        <a target='blank' href='https://geo-jumper-frontend.herokuapp.com/' className='center'><img src={Clouds} alt='cloud-level' /></a>
+        <a target='blank' href='https://ccloops.github.io/quiz-fellows/' className='deg0'><img src={Quiz} alt='quiz fellows'/></a>
+        <a target='blank' href='https://oh-rose.herokuapp.com/' className='deg45'><img src={OhRose} alt='Oh, Rose' /></a>
+        <a target='blank' href='https://github.com/Meetup-Trendly/trendly-server'><img src={Trendly} alt='Trendly'/></a>
+        <a target='blank' href='https://github.com/ccloops/28-todo-app/tree/master/lab-catherine'><img src={Todo} alt='Todo List'/></a>
       </div>
     );
   }
