a1f3f7205616a2a92bdad1e2dafb1ffeb780a754
Catherine Looper <loopercat@gmail.com>
2018-03-29 12:48:53 -0700

    added a resume component

--- src/component/layout/index.js
+++ src/component/layout/index.js
@@ -4,6 +4,7 @@ import {Route, Switch, Redirect, BrowserRouter, Link} from 'react-router-dom';
 
 import Landing from '../landing';
 import Projects from '../projects';
+import Resume from '../resume';
 
 export default class Layout extends React.Component {
   render() {
@@ -14,10 +15,12 @@ export default class Layout extends React.Component {
             <ul>
               <li><Link to='/home' className='nav'>home</Link></li>
               <li><Link to='/projects' className='nav'>projects</Link></li>
+              <li><Link to='/resume' className = 'nav'>resume</Link></li>
             </ul>
           </nav>
           <Switch>
             <Route path='/projects' component={Projects}/>
+            <Route path='resume' component={Resume}/>
             <Route path='/' component={Landing}/>
             <Redirect to='/'/>
           </Switch>
