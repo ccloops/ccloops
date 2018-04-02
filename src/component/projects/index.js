c74a37a917afa576b47ecc39d4ad33d065fb80ea
Catherine Looper <loopercat@gmail.com>
2018-04-02 10:00:45 -0700

    added an image expansion modal

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -11,43 +11,51 @@ import GeoJumper from '../../assets/lava-level-overview.png';
 
 
 export default class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({isExpanded: !this.state.isExpanded});
+  }
   render() {
-
-    // let {project, handleProjectClick} = this.props;
-
-    // let showModal = () => handleProjectClick({...viewing, clicked: true});
-    let showModal = () => console.log('show modal');
-    // let hideModal = () => handleProjectClick({...viewing, clicked: false});
-
-    // let projectClose = (project) => {
-    //   handleProjectClick({...viewing, clicked: false});
-    // };
-
+    console.log(this.state);
     return (
       <div className='projects'>
-        <img onDoubleClick={showModal} src={Clouds} alt='cloud-level' />
-        <p>Geo-Jumper is a multiplayer fullstack platform game that combines socket.io with a frontend React library. This application is designed to create a unique game experience by matching online users to race to the finish line and be the first to catch the star. This application showcases a multiplayer/multi-level game and persists user data using MongoDB.</p>
-        <h3>Geo-Jumper</h3>
-          
+        <div className={this.state.isExpanded ? 'toggleModal' : ''} onDoubleClick={this.toggleModal}>
+          <img src={Clouds} alt='cloud-level' />
+          <p>Geo-Jumper is a multiplayer fullstack platform game that combines socket.io with a frontend React library. This application is designed to create a unique game experience by matching online users to race to the finish line and be the first to catch the star. This application showcases a multiplayer/multi-level game and persists user data using MongoDB.</p>
+          <h3>Geo-Jumper</h3>
+        </div>
     
-        <img onDoubleClick={showModal} src={Quiz} alt='quiz fellows'/>
-        <p>Quiz Fellows is a quiz generating application written in pure vanilla JavaScript. This application allows users to take any of the hard-coded beginner-level coding quizzes in the `Take Quiz` section of the application. The application also allows users to make their very own quizzes in the `Make Quiz` section. These quizzes are then stored so that the user can take their personalized quiz in the same format as the hard-coded quizzes. This application relies on local storage for persistence.</p>
-        <h3>Quiz Fellows</h3>
-          
+        <div className={this.state.isExpanded ? 'toggleModal' : ''} onDoubleClick={this.toggleModal}>
+          <img src={Quiz} alt='quiz fellows'/>
+          <p>Quiz Fellows is a quiz generating application written in pure vanilla JavaScript. This application allows users to take any of the hard-coded beginner-level coding quizzes in the `Take Quiz` section of the application. The application also allows users to make their very own quizzes in the `Make Quiz` section. These quizzes are then stored so that the user can take their personalized quiz in the same format as the hard-coded quizzes. This application relies on local storage for persistence.</p>
+          <h3>Quiz Fellows</h3>
+        </div>
      
-        <img onDoubleClick={showModal} src={OhRose} alt='Oh, Rose' />
-        <p>A React application for a band from Olympia, Washington.</p>
-        <h3>Oh, Rose</h3>
-
+        <div className={this.state.isExpanded ? 'toggleModal' : ''} onDoubleClick={this.toggleModal}>
+          <img src={OhRose} alt='Oh, Rose' />
+          <p>A React application for a band from Olympia, Washington.</p>
+          <h3>Oh, Rose</h3>
+        </div>
    
-        <img onDoubleClick={showModal} src={Trendly} alt='Trendly'/>
-        <p>Trendly is a RESTful API that connects users via (Short Message Service) SMS to Meetup (https://www.meetup.com/). This app allows users to sign up with their Meetup User ID via text message. Once signed up, users will automatically receive a text notification 24 hours before any of their upcoming meetup events. With the Trendly application, users no longer need to worry about missing a Meetup event as text notifications will keep users up to date on all of their Meetup groups.</p>
-        <h3>Trendly</h3>
-
+        <div className={this.state.isExpanded ? 'toggleModal' : ''} onDoubleClick={this.toggleModal}>
+          <img src={Trendly} alt='Trendly'/>
+          <p>Trendly is a RESTful API that connects users via (Short Message Service) SMS to Meetup (https://www.meetup.com/). This app allows users to sign up with their Meetup User ID via text message. Once signed up, users will automatically receive a text notification 24 hours before any of their upcoming meetup events. With the Trendly application, users no longer need to worry about missing a Meetup event as text notifications will keep users up to date on all of their Meetup groups.</p>
+          <h3>Trendly</h3>
+        </div>
     
-        <img onDoubleClick={showModal} src={Todo} alt='Todo List'/>
-        <p>In this project, I built a frontend Todo List application containing a Home Page with a brief description about the application, and a Dashboard page where users can create their own Todo List. To create a Todo List, users can enter a note title and description and click Submit New Note. Users also have the ability to remove any of their created notes by clicking the Delete button next to the note they would like to remove.</p>
-        <h3>To Do List</h3>
+        <div className={this.state.isExpanded ? 'toggleModal' : ''} onDoubleClick={this.toggleModal}>
+          <img src={Todo} alt='Todo List'/>
+          <p>In this project, I built a frontend Todo List application containing a Home Page with a brief description about the application, and a Dashboard page where users can create their own Todo List. To create a Todo List, users can enter a note title and description and click Submit New Note. Users also have the ability to remove any of their created notes by clicking the Delete button next to the note they would like to remove.</p>
+          <h3>To Do List</h3>
+        </div>
       </div>
     );
   }
