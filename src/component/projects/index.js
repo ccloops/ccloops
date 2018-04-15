d8a7256b807851e7b4b421b46264797eac1a6a78
Catherine Looper <loopercat@gmail.com>
2018-04-15 12:50:19 -0700

    added a deployed and github link to each project

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -47,41 +47,45 @@ export default class Projects extends React.Component {
     return (
       <div className='projects'>
         <div className={this.state.isExpanded1 ? 'toggleModal' : ''} onClick={this.toggleModal1}>
-          <a target='blank' href='https://geo-jumper-frontend.herokuapp.com/'>visit</a>
           <img src={Clouds} alt='cloud-level' />
           <p>Geo-Jumper is a multiplayer fullstack platform game that combines socket.io with a frontend React library. This application is designed to create a unique game experience by matching online users to race to the finish line and be the first to catch the star. This application showcases a multiplayer/multi-level game and persists user data using MongoDB.</p>
+          <a target='blank' href='https://geo-jumper-frontend.herokuapp.com/'>deployed</a>
+          <a target='blank' href='https://github.com/geo-jumper'>github</a>
           <h3>GeoJumper</h3>
           <hr/>
         </div>
     
         <div className={this.state.isExpanded2 ? 'toggleModal' : ''} onClick={this.toggleModal2}>
-          <a target='blank' href='https://ccloops.github.io/quiz-fellows/'>visit</a>
           <img src={Quiz} alt='quiz fellows'/>
           <p>Quiz Fellows is a quiz generating application written in pure vanilla JavaScript. This application allows users to take any of the hard-coded beginner-level coding quizzes in the `Take Quiz` section of the application. The application also allows users to make their very own quizzes in the `Make Quiz` section. These quizzes are then stored so that the user can take their personalized quiz in the same format as the hard-coded quizzes. This application relies on local storage for persistence.</p>
+          <a target='blank' href='https://ccloops.github.io/quiz-fellows/'>deployed</a>
+          <a target='blank' href='https://github.com/ccloops/quiz-fellows'>github</a>
           <h3>Quiz Fellows</h3>
           <hr/>          
         </div>
      
         <div className={this.state.isExpanded3 ? 'toggleModal' : ''} onClick={this.toggleModal3}>
-          <a target='blank' href='https://oh-rose.herokuapp.com/'>visit</a>
           <img src={OhRose} alt='Oh, Rose' />
           <p>A React application for a band from Olympia, Washington.</p>
+          <a target='blank' href='https://oh-rose.herokuapp.com/'>deployed</a>
+          <a target='blank' href='https://github.com/ccloops/oh-rose'>github</a>
           <h3>Oh, Rose</h3>
           <hr/>          
         </div>
    
         <div className={this.state.isExpanded4 ? 'toggleModal' : ''} onClick={this.toggleModal4}>
-          <a target='blank' href='https://github.com/Meetup-Trendly/trendly-server'>visit</a>
           <img src={Trendly} alt='Trendly'/>
           <p>Trendly is a RESTful API that connects users via (Short Message Service) SMS to Meetup (https://www.meetup.com/). This app allows users to sign up with their Meetup User ID via text message. Once signed up, users will automatically receive a text notification 24 hours before any of their upcoming meetup events. With the Trendly application, users no longer need to worry about missing a Meetup event as text notifications will keep users up to date on all of their Meetup groups.</p>
+          <a target='blank' href='https://github.com/Meetup-Trendly'>github</a>
           <h3>Trendly</h3>
           <hr/>          
         </div>
     
         <div className={this.state.isExpanded5 ? 'toggleModal' : ''} onClick={this.toggleModal5}>
-          <a target='blank' href='https://catherine-todo.herokuapp.com/'>visit</a>
           <img src={Todo} alt='Todo List'/>
           <p>In this project, I built a frontend Todo List application containing a Home Page with a brief description about the application, and a Dashboard page where users can create their own Todo List. To create a Todo List, users can enter a note title and description and click Submit New Note. Users also have the ability to remove any of their created notes by clicking the Delete button next to the note they would like to remove.</p>
+          <a target='blank' href='https://catherine-todo.herokuapp.com/'>deployed</a>
+          <a target='blank' href='https://github.com/ccloops/28-todo-app'>github</a>
           <h3>Todo List</h3>
           <hr/>          
         </div>
