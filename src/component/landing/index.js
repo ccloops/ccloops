3d37b2c9913f68affaca1d3490994e7bdab391a2
Catherine Looper <loopercat@gmail.com>
2018-04-15 23:12:59 -0700

    added random color to logo

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -10,21 +10,32 @@ export default class Landing extends React.Component {
     super(props);
     this.state = {
       isBlinking: true,
+      backgroundColorCard: '',
     };
 
     this.toggleBlink = this.toggleBlink.bind(this);
+    this.toggleBackgroundColorCard = this.toggleBackgroundColorCard.bind(this);
   }
 
   toggleBlink() {
     this.setState({isBlinking: !this.state.isBlinking});  
   }
+
+  toggleBackgroundColorCard() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    let colorToDisplay = `rgb(${r}, ${g}, ${b})`;
+    this.setState({backgroundColorCard: colorToDisplay});
+  }
   // <div id="yin"></div>
 
   render() {
     return (
-      <div> 
+      <div onClick={this.toggleBackgroundColorCard}> 
         <h1 className={this.state.isBlinking ? 'toggleBlink' : ''} id="blink" onClick={this.toggleBlink}>catherine looper</h1>
-        <img id="logo" src={BusinessCard} />
+        <img onClick={this.toggleBackgroundColorCard} style={{backgroundColor: this.state.backgroundColorCard}} id="logo" src={BusinessCard} />
         
         <footer>
           <a href="/"><h3> © Catherine Looper 2018</h3></a>
